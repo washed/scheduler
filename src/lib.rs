07c658e164832ef709997c4eecbf462b7dc235bb
Marc Freudenberg <freudenbergmarc@gmail.com>
2023-11-21 08:45:51 +0100

    fix disabled filter

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Datelike, Duration, DurationRound, Local, TimeZone, Utc, Date};
+use chrono::{Date, DateTime, Datelike, Duration, DurationRound, Local, TimeZone, Utc};
 
 struct Weekly {
     pub weekdays: [bool; 7],
@@ -16,18 +16,24 @@ impl Weekly {
             .iter()
             .cycle()
             .enumerate()
-            .filter(|(_i, e)| **e)
             .skip(now().weekday().num_days_from_monday() as usize)
-            .map(move |(i, _e)| {
+            .map(move |(i, e)| {
                 let now = now();
                 let weekday = now.weekday();
                 let weekday_offset = weekday.num_days_from_monday() as i64;
                 let now_midnight = now.duration_round(Duration::days(1)).unwrap();
-                now_midnight + Duration::days(i as i64 - weekday_offset) + self.time
+                (
+                    *e,
+                    now_midnight + Duration::days(i as i64 - weekday_offset) + self.time,
+                )
             })
-            .skip_while(move |dt| {
+            .skip_while(move |(_e, dt)| {
                 let now = now();
                 *dt < now
+            })
+            .filter_map(|(e, dt)| match e {
+                true => Some(dt),
+                false => None,
             })
             .take(n)
     }
@@ -50,11 +56,15 @@ mod tests {
     use hhmmss::Hhmmss;
 
     fn fake_now_utc() -> DateTime<Utc> {
-        DateTime::parse_from_rfc3339("2023-01-01T00:00:00Z").unwrap().into()
+        DateTime::parse_from_rfc3339("2023-01-01T00:00:00Z")
+            .unwrap()
+            .into()
     }
 
     fn fake_now_local() -> DateTime<Local> {
-        DateTime::parse_from_rfc3339("2023-01-01T00:00:00+01:00").unwrap().into()
+        DateTime::parse_from_rfc3339("2023-01-01T00:00:00+01:00")
+            .unwrap()
+            .into()
     }
 
     #[test]
@@ -64,22 +74,39 @@ mod tests {
             time: Duration::hours(12),
         };
 
-        let ttnr: Vec<DateTime<Utc>> = weekly.next_runs(8, fake_now_utc).collect();
+        let ttnr: Vec<DateTime<Utc>> = weekly.next_runs(9, fake_now_utc).collect();
         let expected_ttnr: Vec<DateTime<Utc>> = [
-            DateTime::parse_from_rfc3339("2023-01-01T12:00:00Z").unwrap().into(),
+            DateTime::parse_from_rfc3339("2023-01-01T12:00:00Z")
+                .unwrap()
+                .into(),
             // 2nd skipped because it's a monday
-            DateTime::parse_from_rfc3339("2023-01-03T12:00:00Z").unwrap().into(),
-            DateTime::parse_from_rfc3339("2023-01-04T12:00:00Z").unwrap().into(),
-            DateTime::parse_from_rfc3339("2023-01-05T12:00:00Z").unwrap().into(),
-            DateTime::parse_from_rfc3339("2023-01-06T12:00:00Z").unwrap().into(),
-            DateTime::parse_from_rfc3339("2023-01-07T12:00:00Z").unwrap().into(),
-            DateTime::parse_from_rfc3339("2023-01-08T12:00:00Z").unwrap().into(),
+            DateTime::parse_from_rfc3339("2023-01-03T12:00:00Z")
+                .unwrap()
+                .into(),
+            DateTime::parse_from_rfc3339("2023-01-04T12:00:00Z")
+                .unwrap()
+                .into(),
+            DateTime::parse_from_rfc3339("2023-01-05T12:00:00Z")
+                .unwrap()
+                .into(),
+            DateTime::parse_from_rfc3339("2023-01-06T12:00:00Z")
+                .unwrap()
+                .into(),
+            DateTime::parse_from_rfc3339("2023-01-07T12:00:00Z")
+                .unwrap()
+                .into(),
+            DateTime::parse_from_rfc3339("2023-01-08T12:00:00Z")
+                .unwrap()
+                .into(),
             // 9th skipped because it's a monday
-            DateTime::parse_from_rfc3339("2023-01-10T12:00:00Z").unwrap().into(),
-            DateTime::parse_from_rfc3339("2023-01-11T12:00:00Z").unwrap().into(),
-        ].into();
-
-        // TODO: I'd expect this test to pass, but for some reason we are skipping 2 days instead of one. Debug!
+            DateTime::parse_from_rfc3339("2023-01-10T12:00:00Z")
+                .unwrap()
+                .into(),
+            DateTime::parse_from_rfc3339("2023-01-11T12:00:00Z")
+                .unwrap()
+                .into(),
+        ]
+        .into();
 
         assert_eq!(ttnr, expected_ttnr);
         for d in ttnr {
