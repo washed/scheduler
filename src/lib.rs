055e604381877629fcfb4cb5023ddbedd9ec7d28
Marc Freudenberg <freudenbergmarc@gmail.com>
2024-01-02 15:07:32 +0100

    wtf

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,22 @@ pub mod trigger;
 
 #[macro_use]
 pub mod job;
+
+pub fn increment(x: u32) -> u32 {
+    x + 1
+}
+
+#[macro_export]
+macro_rules! inc_a {
+    ($x:expr) => ( ::increment($x) )
+}
+
+#[macro_export]
+macro_rules! inc_b {
+    ($x:expr) => ( ::scheduler::increment($x) )
+}
+
+#[macro_export]
+macro_rules! inc {
+    ($x:expr) => ( $crate::increment($x) )
+}
